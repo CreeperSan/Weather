05278a05f20e4131aefb7f87fe14898ae22abd8e
CreeperSan <sanc876@gmail.com>
2017-05-17 17:21:32 +0800

    .

---
+++ app/src/main/java/creeper_san/weather/Fragment/MainPrefFragment.java
@@ -0,0 +1,18 @@
+package creeper_san.weather.Fragment;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainPrefFragment extends PreferenceFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+}
